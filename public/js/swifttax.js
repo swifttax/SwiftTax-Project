f03c180cd599117b57ffe7091655e49fb7baac4c
Kendrick Gardner <kbgar24@gmail.com>
2016-03-07 18:34:55 -0600

    daily update

--- public/js/swifttax.js
+++ public/js/swifttax.js
@@ -60,11 +60,11 @@ $(".nextBtn1").hide();
     if ((/[A-Z]/ig).test($("#inputFirstName").val())) {
         $("#validNameCheck").hide()
         if ($("#inputFirstName").val().length > 1) {
-          $(".nextBtn1").fadeIn(1000).css("transition: ''");
+          $(".nextBtn1").fadeIn(300).css("transition: ''");
         }
     }
     if (($("#inputFirstName").val().length < 2)) {
-      $(".nextBtn1").fadeOut(1000).css("transition: ''");
+      $(".nextBtn1").fadeOut(300).css("transition: ''");
        $("#validNameCheck").show() 
     }
     if (!((/[A-Z]/ig).test($("#inputFirstName").val()))) {
@@ -290,9 +290,9 @@ $(".nextBtn1").hide();
       $("#loopGif").hide();
       $("#text_16_4").fadeIn();
       $("#finalProgressDiv").show()
-      $(".nextBtnOwe").fadeIn(2000);
-      $(".nextBtnRefund").fadeIn(2000);
-      $(".backBtn16").fadeIn(2000);
+      $(".nextBtnOwe").fadeIn();
+      $(".nextBtnRefund").fadeIn();
+      $(".backBtn16").fadeIn();
     },12000);
     
     
@@ -336,18 +336,20 @@ $(".nextBtn1").hide();
 
       $(".nextBtnOwe").click(function(){
     $('#modal16').closeModal({out_duration: 0,});
-    window.scrollTo(0,0);
     $(".taxform").hide();
     $("#coolComputer").hide();
+    $(".macDaddy").hide();
     $('#modal17').show();
+    $('html,body').scrollTop(0);
     // $('.lean-overlay').hide();
 });
       $(".nextBtnRefund").click(function(){
     $('#modal16').closeModal({out_duration: 0,});
-    window.scrollTo(0,0);
     $(".taxform").hide();
     $("#coolComputer").hide();
+    $(".macDaddy").hide();
     $('#modal19').show();
+     $('html,body').scrollTop(0);
 
     
     // $('.lean-overlay').hide();
@@ -569,7 +571,8 @@ myDataRef.on('value', function(snapshot) {
     remainingRefund2 = (refund - (data[randomFirst].price));
     if (basket.length < 3) {
         pickRandomSecond();
-        if ((randomSecond != randomFirst) && ((remainingRefund2 - data[randomSecond].price) > 10)){
+        console.log(randomSecond);
+        if (randomSecond != randomFirst){
               basket.push(randomSecond);
               return
             }
@@ -582,6 +585,7 @@ myDataRef.on('value', function(snapshot) {
     remainingRefund3 = (refund - ((data[randomFirst].price) + data[randomSecond].price));
     if (basket.length < 3) {
         pickRandomThird();
+        console.log(randomThird);
         if (randomThird != randomSecond){
             basket.push(randomThird);
             console.log("remainFinal " + (refund-((parseFloat(data[basket[0]].price) + parseFloat(data[basket[1]].price) + parseFloat(data[basket[2]].price)))));
@@ -605,7 +609,9 @@ myDataRef.on('value', function(snapshot) {
 
   function amazonRequest(){
     fillBasket();
-
+    $(".amazonBox1").hide();
+    $(".amazonBox2").hide();
+    $(".amazonBox3").hide();
     basket.forEach(function(element, index){
         $("#amazonText" + index).text(data[basket[index]].name);
         $("#amazonImg" + index).attr("src", data[basket[index]].image);
@@ -621,9 +627,20 @@ myDataRef.on('value', function(snapshot) {
           width: 77
         }));
         }
+        console.log(data[basket[index]].price);
+        console.log((data[basket[index]].price).toFixed(2));
         $("#price" + index).text("$" + (data[basket[index]].price).toFixed(2));
         $("#url" + index).attr("href", data[basket[index]].url );
     });
+    setTimeout(function(){
+      $(".amazonBox1").fadeIn(800);
+    },100);
+     setTimeout(function(){
+      $(".amazonBox2").fadeIn(800);
+    },600);
+      setTimeout(function(){
+      $(".amazonBox3").fadeIn(800);
+    },1100);
   };
 
   amazonRequest();
